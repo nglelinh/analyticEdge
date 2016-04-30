5439c21d5fd66d5864935c5802b68777d7c5db1d
nglelinh <nglelinh@gmail.com>
2016-04-30 16:31:14 +0700

    first week

---
+++ week1/The Analytics Edge.md
@@ -0,0 +1,56 @@
+The Analytics Edge
+==================
+
+1. https://github.com/pedrosan/TheAnalyticsEdge
+2. https://github.com/TarekDib03/Analytics
+3. https://github.com/leanton/15.071x
+
+## Unit 1: An introduction to analytic
+
+Introduction to R
+
+some examples: IBM watson, eHarmony, heart disease, ...
+
+ls() --- list all variables (whos in octave)
+
+create vector: c()
+
+add new observations to a data frame: **rbind** -->> merge many data frame
+
+combine two vectors into a data frame: data.frame
+
+add or retreave a variable to your data frame: dollar sign -> dataframe$column
+
+read.csv()
+
+write.csv()
+
+str(dataframe): show structure
+
+subset(dataframe, condition): get sub-dataframe
+
+sd(): standard deviation
+
+mean()
+
+rm(): remove
+
+summary()
+
+which.min, which.max
+
+plot(dataframe@column1, datafram@column2)
+
+nrow(): number of row
+
+hist()
+
+boxplot()
+
+table(dataframe$column) -->> number of each value
+
+tapply -- split by second arg, apply the third arg to each of first arg
+
+
+
+
---
+++ week1/Unit1_Assignment.R
@@ -0,0 +1,82 @@
+# Get the current directory
+  getwd()
+# Read the csv file
+  mvt = read.csv("mvtWeek1.csv")
+# Structure of the dataset
+  str(mvt)
+# Statistical summary
+  summary(mvt)
+# Number of rows
+  nrow(mvt)
+
+# Max of ID
+  max(mvt$ID)
+
+# How many observations have value TRUE in the Arrest variable 
+  table(mvt$Arrest)
+
+# converts the variable "Date" into a Date object in R
+  DateConvert = as.Date(strptime(mvt$Date, "%m/%d/%y %H:%M"))
+
+# extract the month and the day of the week, and add these variables to our data frame mvt
+  mvt$Month = months(DateConvert)
+  mvt$Weekday = weekdays(DateConvert)
+
+# replace the old Date variable with DateConvert 
+  mvt$Date = DateConvert
+
+# which month did the fewest motor vehicle thefts occur
+  table(mvt$Month)
+
+# Which month has the largest number of motor vehicle thefts for which an arrest was made
+  mvtArrested = subset(mvt, mvt$Arrest == TRUE)
+  table(mvtArrested$Month)
+
+# histogram of the variable Date
+  hist(mvt$Date, breaks=100)
+
+# Create a boxplot of the variable "Date", sorted by the variable "Arrest" 
+  boxplot(mvt$Date ~ mvt$Arrest)
+
+# proportion of motor vehicle thefts in 2001 was an arrest made
+  table(mvtArrested$Year)
+  table(mvt$Year)
+
+# Which locations are the top five locations for motor vehicle thefts, excluding the "Other" category?
+  sort(table(mvt$LocationDescription))
+
+# Create a subset of your data, only taking observations for which the theft happened in one of these five locations
+  Top5 = subset(mvt, mvt$LocationDescription == "STREET" | mvt$LocationDescription == "PARKING LOT/GARAGE(NON.RESID.)" | mvt$LocationDescription == "ALLEY" | mvt$LocationDescription == "GAS STATION" | mvt$LocationDescription == "DRIVEWAY - RESIDENTIAL")
+
+# refactor for new subset
+  Top5$LocationDescription = factor(Top5$LocationDescription)
+
+# highiest arrest rate
+  Top5Arrested = subset(Top5, Top5$Arrest == 'TRUE')
+  table(Top5$LocationDescription)
+  table(Top5Arrested$LocationDescription)
+
+##############################################################################
+  IBM = read.csv("IBMStock.csv")
+  GE = read.csv("GEStock.csv")
+  ProcterGamble = read.csv("ProcterGambleStock.csv")
+  Boeing = read.csv("BoeingStock.csv")
+  CocaCola = read.csv("CocaColaStock.csv")
+
+
+  IBM$Date = as.Date(IBM$Date, "%m/%d/%y")
+  GE$Date = as.Date(GE$Date, "%m/%d/%y")
+  CocaCola$Date = as.Date(CocaCola$Date, "%m/%d/%y")
+  ProcterGamble$Date = as.Date(ProcterGamble$Date, "%m/%d/%y")
+  Boeing$Date = as.Date(Boeing$Date, "%m/%d/%y")  
+
+  # Visualizing Stock Dynamics
+  plot(CocaCola$Date, CocaCola$StockPrice, type="l", col="red")
+  lines(ProcterGamble$Date, ProcterGamble$StockPrice, col="blue")
+
+  # Vertical line
+  abline(v=as.Date(c("2000-03-01")), lwd=2)
+  abline(v=as.Date(c("1983-01-01")), lwd=2)
+
+  # how the stock prices changed from 1995-2005 for all five companies
+  plot(CocaCola$Date[301:432], CocaCola$StockPrice[301:432], type="l", col="red", ylim=c(0,210))
